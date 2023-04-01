350ae2b1791e0f3f4ce8aa9b4d144a9cab7d1c11
uttamshukla259 <117875151+uttamshukla259@users.noreply.github.com>
2023-04-01 20:27:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+function countWords() {
+  var text = document.getElementById('evaluatedText').value;
+  var wordCount = 0;
+  if (text.trim() !== '') {
+    var matches = text.match(/\b\w+\b/g);
+    wordCount = matches ? matches.length : 0;
+  }
+  document.getElementById('wordCount').textContent = wordCount;
+}
